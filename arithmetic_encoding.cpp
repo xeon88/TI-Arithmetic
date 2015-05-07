31ac9a3b32dfe63e08a7807faa5df8f1eb62a297
xeon88 <theacidmc@gmail.com>
2015-05-07 15:03:24 +0200

    source ok

--- arithmetic_encoding.cpp
+++ arithmetic_encoding.cpp
@@ -13,30 +13,38 @@ psym * make_source()
     int rd; // temporanea per assegnazione probabilità
     int s=0; // inizio range valori estrazione
     int e=0; // fine range valori estrazione
-    int cumulative=0 ; // verifica somma probabilità
+    int * ranges= new int [NUM_SYM];
+
 
     for(int i=0; i<NUM_SYM;i++)
     {
-        rd = rand() % tot_prob;
-        p=(double)rd/TOT;
-        symb=BASE_SYM+i;
+
+        rd =(rand() % (tot_prob/NUM_SYM));
+        ranges[i]=rd;
         tot_prob -=rd;
-        e=s+rd;
-        s=e+1;
-        vect[i]= new sym(symb,p,s,e);
-        cumulative +=rd;
     }
 
-    if(cumulative<TOT) // la somma della cumulata non è uguale alla totalità
+    if(tot_prob<TOT) // la somma della cumulata non è uguale alla totalità
     {
-        int rest=TOT-cumulative;
-        double inc=(double)(rest/(TOT*NUM_SYM));
-        for (int i=0;i<NUM_SYM;i++)
+        int j=0;
+        while(tot_prob>0)
         {
-            vect[i]->p += inc;
+            ranges[j%NUM_SYM]++;
+            j++;
+            tot_prob--;
         }
 
     }
+
+    for (int k=0;k<NUM_SYM;k++)
+    {
+        e=s+ranges[k]-1;
+        p=(double)ranges[k]/(double)TOT;
+        vect[k]=new sym(BASE_SYM+k,p,s,e);
+        s=e+1;
+    }
+
+
 
     return vect;
 }
@@ -67,11 +75,32 @@ int main(int argc,char** argv)
         cout << "=================== \n";
         cout << "simbolo : " << vect[i]->val << " \n";
         cout << "probabilità : " << vect[i]->p << " \n";
+        cout << "start :" << vect[i]->start << "\n";
+        cout << "end :" << vect[i]->end << "\n";
         cout << "=================== \n";
 
     }
 
     cout << "Probabilità cumulativa :" << totp << "\n";
 
+    int * counters = new int[NUM_SYM];
+    psym extract;
+
+    for(int i=0;i<1000;i++)
+    {
+
+        extract=select_sym(vect);
+        counters[extract->val]++;
+
+    }
+
+    for(int i=0;i<NUM_SYM;i++)
+    {
+        cout << "simbolo : " << i << "Num estrazioni : " << counters[i] << "\n";
+    }
+
     return 0;
 }
+
+
+
