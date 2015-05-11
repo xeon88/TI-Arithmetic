880c954494d67a80cb24909af5062bfe68ed8822
xeon88 <theacidmc@gmail.com>
2015-05-11 22:38:39 +0200

    encode decode ok

--- arithmetic_encoding.cpp
+++ arithmetic_encoding.cpp
@@ -1,19 +1,27 @@
 #include "arithmetic_encoding.h"
 
 
-sym::sym(int in, double pin, int s , int e) : val(in) , p(pin) , start(s) , end(e)  {}
+sym::sym(char in, double pin, int s , int e, double sp, double ep) :
+    val(in) , p(pin) , start(s) , end(e) , start_p(sp), end_p(ep) {}
 
-psym * make_source()
+/*
+    Funzione per la generazione del modello della sorgente , ovvero i simboli
+    e le loro probabilità di emissione
+*/
+
+
+psym * make_source_model()
 {
 
     psym * vect= new psym[NUM_SYM];
-    int tot_prob=TOT; // probabilità ancora da assegnare
-    double p;  // probabilità del simbolo
-    int symb;   // valore simbolo numerico
-    int rd; // temporanea per assegnazione probabilità
-    int s=0; // inizio range valori estrazione
-    int e=0; // fine range valori estrazione
-    int * ranges= new int [NUM_SYM];
+    int tot_prob=TOT;                       // probabilità ancora da assegnare
+    double p;                               // probabilità del simbolo
+    int rd;                                 // temporanea per assegnazione probabilità
+    int s=0;                                // inizio range valori estrazione
+    int e=0;                                // fine range valori estrazione
+    double sp;                              // probabilità iniziale
+    double ep;                              // probabilità finale
+    int * ranges= new int [NUM_SYM];        // vettore con le dimensione dei range di estrazione
 
 
     for(int i=0; i<NUM_SYM;i++)
@@ -24,7 +32,7 @@ psym * make_source()
         tot_prob -=rd;
     }
 
-    if(tot_prob<TOT) // la somma della cumulata non è uguale alla totalità
+    if(tot_prob<TOT)                        // distribuzione dei range non completa
     {
         int j=0;
         while(tot_prob>0)
@@ -39,9 +47,11 @@ psym * make_source()
     for (int k=0;k<NUM_SYM;k++)
     {
         e=s+ranges[k]-1;
-        p=(double)ranges[k]/(double)TOT;
-        vect[k]=new sym(BASE_SYM+k,p,s,e);
-        s=e+1;
+        p=(double)ranges[k]/(double)TOT;                // probabilità del simbolo
+        ep=(double)e/(double)TOT;
+        sp=(double)s/(double)TOT;
+        vect[k]=new sym(BASE_SYM+k,p,s,e,sp,ep);
+        s=e+1;                                          // aggiorno il valore per lo start
     }
 
 
@@ -49,12 +59,11 @@ psym * make_source()
     return vect;
 }
 
-psym select_sym(psym * vect)
+psym select_sym(psym * vect, int rand)
 {
-    int rd= rand() % TOT;
     int i=0;
 
-    while(rd<vect[i]->start || rd>vect[i]->end) // cerca in quale simbolo ricade rd
+    while(rand<vect[i]->start || rand>vect[i]->end)     // cerca in quale simbolo ricade rd
     {
         i++;
     }
@@ -62,11 +71,95 @@ psym select_sym(psym * vect)
     return vect[i];
 }
 
+/*
+    Compone una stringa la quale termina sempre con il simbolo di fine stringa
+    definito come ultimo elemento del vettore vect EOS = vect[NUM_SYM-1]
+*/
+
+char* make_message(int n, psym * vect)
+{
+
+    int k;                                          // variabile per estrazione int;
+    char * message = new char[n];                   // messaggio da produrre
+    psym extract;
+    int range_sym= TOT-(EOS->end - EOS->start +1);   // escludo il fine stringa
+    for(int i=0;i<n-1;i++)                          // compongo casualmente il messaggio
+    {
+        k=rand()%range_sym;
+        extract=select_sym(vect,k);
+        message[i]=extract->val;
+    }
+
+    message[n-1]=EOS->val;                          // concludo con il fine stringa
+
+    return message;
+
+}
+
+/*
+calcola un valore double il quale esprime la codifica della stringa
+
+*/
+
+double encode_message(int n , char * message , psym * vect)
+{
+
+    double encode;
+    double high_p=1;                            // inizializzo estremo superiore
+    double low_p=0;                             // inizializzo estremo inferiore
+    double range=0;
+
+    for(int i=0;i<n;i++)
+    {
+        char curr_char=message[i];
+        psym curr_sym=vect[(int)curr_char-BASE_SYM];
+        range=high_p-low_p;
+        high_p=low_p+(range)*(curr_sym->end_p);            // aggiorno estremo sup range prob
+        low_p=low_p+(range)*(curr_sym->start_p);           // aggiorno estremo inf prob
+    }
+
+    encode=(high_p+low_p)/2;            // scelgo per semplificità il punto medio dell'intervallo
+    return encode;
+
+}
+
+/*
+    Decodifica il messaggio originale avendo in input il double calcolato dall'encoder
+
+*/
+
+char * decode_message(double p, int n, psym * vect)
+{
+
+    char * message=new char[n];
+    double low=0;           // lower bound
+    double high=1;          // upper bound
+    double range;           // dimensione range su cui ricercare p
+    int j;
+    double old_low;         // tmp per aggiornamento del lower bound con cui poi calcolo upper
+    for(int i=0;i<n;i++)
+    {
+        j=0;
+        range=high-low;                                 // aggiorno la dimensione del range
+        while(!((p>low+(range)*vect[j]->start_p) &&
+                (p<low+(range)*vect[j]->end_p)) )       //  cerco j t.c low(j)< p < high(j)
+        {
+            j++;
+        }
+
+        message[i]=vect[j]->val;
+        old_low=low;
+        low=low+(range)*(vect[j]->start_p);
+        high=old_low+(range)*(vect[j]->end_p);
+    }
+
+    return message;
+}
 
 int main(int argc,char** argv)
 {
 
-    psym * vect = make_source();
+    psym * vect = make_source_model();
     double totp=0;
 
     for(int i=0 ; i<NUM_SYM;i++)
@@ -75,29 +168,27 @@ int main(int argc,char** argv)
         cout << "=================== \n";
         cout << "simbolo : " << vect[i]->val << " \n";
         cout << "probabilità : " << vect[i]->p << " \n";
-        cout << "start :" << vect[i]->start << "\n";
-        cout << "end :" << vect[i]->end << "\n";
+        cout << "start :" << vect[i]->start_p << "\n";
+        cout << "end :" << vect[i]->end_p << "\n";
         cout << "=================== \n";
 
     }
 
-    cout << "Probabilità cumulativa :" << totp << "\n";
 
-    int * counters = new int[NUM_SYM];
-    psym extract;
+    int n=10;
+    double encode;
+    char * send = make_message(n,vect);
+    char * received;
 
-    for(int i=0;i<1000;i++)
-    {
+    cout << "Message to encode : " << send << "\n";
 
-        extract=select_sym(vect);
-        counters[extract->val]++;
+    encode=encode_message(n,send,vect);
 
-    }
+    cout << "Result encode : " << encode << "\n";
 
-    for(int i=0;i<NUM_SYM;i++)
-    {
-        cout << "simbolo : " << i << "Num estrazioni : " << counters[i] << "\n";
-    }
+    received=decode_message(encode,n,vect);
+
+    cout << "Message decoded : " << received << "\n";
 
     return 0;
 }
--- arithmetic_encoding.h
+++ arithmetic_encoding.h
@@ -1,23 +1,27 @@
 #include <stdio.h>
 #include <iostream>
 #include <stdlib.h>
-#define NUM_SYM 20
-#define BASE_SYM 0
+#define NUM_SYM 10
+#define BASE_SYM 99
 #define TOT 1000
+#define EOS vect[NUM_SYM-1]
 
 using namespace std;
 
 typedef struct sym
 {
-    int val; // simbolo numero
+    char val; // simbolo numero
     double p; // probabilità
     int start; // inizio range valori estrazione
     int end; // fine range valori estrazione
-    sym(int val , double p, int s , int e);
+    double start_p; // start range probabilità
+    double end_p; // end range probabilità
+    sym(char val , double p, int s , int e , double sp, double ep);
     ~sym();
 };
 
 typedef struct sym *  psym;
 
-psym * make_source();
-psym select_sym(psym * vect);
+psym * make_source_model(); // genera il modello per la sorgente
+psym select_sym(psym * vect, int rand); // seleziona il simbolo rappresentato da rand
+char* make_message(int n, psym * symbols); // genera il messaggio casuale;
